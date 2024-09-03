5c1d4d7df128325c1cea71a6efbe2538fd0d6a08
Grant <acdcgrr@gmail.com>
2024-09-03 08:54:09 -0500

    switched to using CMake

---
+++ mingwBuild.bat
@@ -0,0 +1,2 @@
+cmake -S . -B mingwBuild -G "MinGW Makefiles" -DVCPKG_TARGET_TRIPLET=x64-mingw-dynamic
+echo using Mingw Makefiles CMake generator
---
+++ msvcBuild.bat
@@ -0,0 +1,2 @@
+cmake -S . -B VSBuild -DVCPKG_TARGET_ARCHITECTURE=x64
+echo using the default cmake generator (visual studio on windows)
